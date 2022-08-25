efe92f9400ff401d1bdc099b1978ad375d509721
Sayed <sayedayman2427@gmail.com>
2022-08-25 22:28:10 +0200

    update contact page
    add Frequently Section and Logo Container Section

--- js/script.js
+++ js/script.js
@@ -119,10 +119,49 @@ var swiper = new Swiper(".reviews-slider", {
     }
 });
 
-// 
+// Swiper for Slider Logo Container in About page
+var swiper = new Swiper(".logo-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        450:{
+            slidesPerView:2,
+        },
+        768:{
+            slidesPerView:3,
+        },
+        991:{
+            slidesPerView:4,
+        },
+        1200:{
+            slidesPerView:5,
+        }
+    }
+});
+
+// Swiper for Frequently Asked Questions To Show p pharagraph text
+let accordion = document.querySelectorAll('.freq .accordion-container .accordion');
+
+accordion.forEach(acco => {
+    acco.onclick = () => {
+        accordion.forEach(dion => dion.classList.remove('active'));
+        acco.classList.toggle('active');
+    };
+});
+
+// Swiper for Button To Show image
 document.querySelector('.load-more .btn').onclick = () => {
     document.querySelectorAll('.courses .box-container .hide').forEach(show => {
         show.style.display = 'block';
     });
     document.querySelector('.load-more .btn').style.display = 'none';
 };
+
+
+
+
+
